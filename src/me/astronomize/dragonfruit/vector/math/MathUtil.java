fb57b68ecaf736bf9fc781e67a19bb98459af2b7
astronomizestudios <51801389+astronomizestudios@users.noreply.github.com>
2019-07-10 13:25:27 -0600

    Update to 0.00.02
    Deprecated the math functions, please use the ones in your vector!

--- src/me/astronomize/dragonfruit/vector/math/MathUtil.java
+++ src/me/astronomize/dragonfruit/vector/math/MathUtil.java
@@ -4,12 +4,21 @@ import me.astronomize.dragonfruit.vector.Vector2;
 import me.astronomize.dragonfruit.vector.Vector3;
 import me.astronomize.dragonfruit.vector.Vector4;
 
+/**
+ * <pre>
+ * MathUtil
+ * For handling vecor math.
+ * </pre>
+ * @author Jeremy Pace
+ *
+ */
 public class MathUtil {
 	
 	/**
 	 * Multiplies the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector2 multiply(Vector2 arg0, Vector2 arg1) {
 		return new Vector2(arg0.getX() * arg1.getX(), arg0.getY() * arg1.getY());
@@ -19,6 +28,7 @@ public class MathUtil {
 	 * Multiplies the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector3 multiply(Vector3 arg0, Vector3 arg1) {
 		return new Vector3(arg0.getX() * arg1.getX(), arg0.getY() * arg1.getY(), arg0.getZ() * arg1.getZ());
@@ -28,6 +38,7 @@ public class MathUtil {
 	 * Multiplies the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector4 multiply(Vector4 arg0, Vector4 arg1) {
 		return new Vector4(arg0.getX() * arg1.getX(), arg0.getY() * arg1.getY(), arg0.getZ() * arg1.getZ(), arg0.getW() * arg1.getW());
@@ -39,6 +50,7 @@ public class MathUtil {
 	 * Subtracts the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector2 subtract(Vector2 arg0, Vector2 arg1) {
 		return new Vector2(arg0.getX() * arg1.getX(), arg0.getY() * arg1.getY());
@@ -48,6 +60,7 @@ public class MathUtil {
 	 * Subtracts the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector3 subtract(Vector3 arg0, Vector3 arg1) {
 		return new Vector3(arg0.getX() - arg1.getX(), arg0.getY() - arg1.getY(), arg0.getZ() - arg1.getZ());
@@ -57,6 +70,7 @@ public class MathUtil {
 	 * Subtracts the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector4 subtract(Vector4 arg0, Vector4 arg1) {
 		return new Vector4(arg0.getX() - arg1.getX(), arg0.getY() - arg1.getY(), arg0.getZ() - arg1.getZ(), arg0.getW() - arg1.getW());
@@ -68,6 +82,7 @@ public class MathUtil {
 	 * Divides the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector2 divide(Vector2 arg0, Vector2 arg1) {
 		return new Vector2(arg0.getX() / arg1.getX(), arg0.getY() / arg1.getY());
@@ -77,6 +92,7 @@ public class MathUtil {
 	 * Divides the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector3 divide(Vector3 arg0, Vector3 arg1) {
 		return new Vector3(arg0.getX() / arg1.getX(), arg0.getY() / arg1.getY(), arg0.getZ() / arg1.getZ());
@@ -86,6 +102,7 @@ public class MathUtil {
 	 * Divides the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector4 divide(Vector4 arg0, Vector4 arg1) {
 		return new Vector4(arg0.getX() / arg1.getX(), arg0.getY() / arg1.getY(), arg0.getZ() / arg1.getZ(), arg0.getW() / arg1.getW());
@@ -97,6 +114,7 @@ public class MathUtil {
 	 * Adds the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
 	 * @since 0.00.01
+	 * @deprecated
 	 */
 	public static Vector2 add(Vector2 arg0, Vector2 arg1) {
 		return new Vector2(arg0.getX() + arg1.getX(), arg0.getY() + arg1.getY());
@@ -105,6 +123,7 @@ public class MathUtil {
 	/**
 	 * Adds the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
+	 * @deprecated Use the add function in the vector class!
 	 * @since 0.00.01
 	 */
 	public static Vector3 add(Vector3 arg0, Vector3 arg1) {
@@ -114,6 +133,7 @@ public class MathUtil {
 	/**
 	 * Adds the variables of the 2 specified vectors, and returns a new vector with the
 	 * final results.
+	 * @deprecated
 	 * @since 0.00.01
 	 */
 	public static Vector4 add(Vector4 arg0, Vector4 arg1) {
