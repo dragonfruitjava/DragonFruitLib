b5fe8a90fe471661172fb42415b3d273fea7c96c
astronomizestudios <51801389+astronomizestudios@users.noreply.github.com>
2019-07-10 13:21:56 -0600

    Update to 0.00.02

--- src/me/astronomize/dragonfruit/DragonFruit.java
+++ src/me/astronomize/dragonfruit/DragonFruit.java
@@ -1,7 +1,7 @@
 package me.astronomize.dragonfruit;
 
 public class DragonFruit {
-	private static String version = "0.0.01";
+	private static String version = "0.0.02";
 
 	public static String getVersion() {
 		return version;
