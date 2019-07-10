efb1aa87a6133bebf6a4ae49d91ba8d03922dff0
astronomizestudios <51801389+astronomizestudios@users.noreply.github.com>
2019-07-09 22:11:29 -0600

    Change @since values

--- src/me/astronomize/dragonfruit/longutil/LongUtil.java
+++ src/me/astronomize/dragonfruit/longutil/LongUtil.java
@@ -1,12 +1,17 @@
 package me.astronomize.dragonfruit.longutil;
 
+/**
+ * Utilities for handling integers.
+ *
+ * @author astronomize
+ */
 public class LongUtil {
 	
 	/**
 	 * <pre>
 	 * returns true if the specified string can be parsed to a long, and false if it can't.
 	 * </pre>
-	 * @since 0.00.00a
+	 * @since 0.00.00
 	 */
 	public static boolean canParseToLong(String s) {
 		try {
